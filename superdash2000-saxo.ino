8ad5553f71f85bc79826239df76af5983a081e53
Thomas Antonini <antonini.thomas@gmail.com>
2022-03-05 13:26:12 +0100

    Add mode management
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- superdash2000-saxo.ino
+++ superdash2000-saxo.ino
@@ -8,6 +8,12 @@ const int lcd_width = 16; /* LCD width in characters */
 const int loop_period = 300;  /* Main loop period */
 const int pin_button = 0x2; /* Digital pin for push button */
 const int welcome_screen_duration_ms = 2000;  /* Welcome scren duration in ms */
+/* This enum is used for the order of the modes.
+   Just reorganize the enum for modifying the order.
+   Modes can also be removed from this enum to not use them */
+enum mode {
+  MODE_MAX
+};
 
 /* Constants */
 const int adc_resolution = 1024;
@@ -37,7 +43,9 @@ const byte lcd_char_full[] = {
 
 /* Globals */
 float adc_reference;
+bool change_mode = false;
 LiquidCrystal_I2C lcd(lcd_i2c_addr, lcd_width, lcd_height);
+int mode = 0;
 
 void setup() {
   if (config_serial) {
@@ -87,7 +95,13 @@ void loop() {
   unsigned long t1;
   unsigned long t2;
   t1 = millis();
-  /* Things to do during loop */
+  if (change_mode) {
+    lcd.clear();  /* Clear the LCD when changing mode to avoid undesired characters */
+    change_mode = false;
+  }
+
+  /* Run the selected mode */
+
   t2 = millis();
 
   if ((t2 - t1) < loop_period) {
@@ -105,6 +119,12 @@ void button_irq_handler(void) {
     if (config_serial) {
       Serial.println("Button pushed");
     }
+
+    mode++;
+    if (mode >= MODE_MAX) {
+      mode = 0;
+    }
+    change_mode = true;
   }
 
   last_interrupt_time = interrupt_time;
