78fa3f7b8585e273061fd8239b2adf07fdbaf130
Thomas Antonini <antonini.thomas@gmail.com>
2022-03-05 14:05:34 +0100

    Add welcome screen
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- superdash2000-saxo.ino
+++ superdash2000-saxo.ino
@@ -3,6 +3,7 @@
 /* Configuration */
 const int lcd_height = 2; /* LCD height in characters */
 const int lcd_width = 16; /* LCD width in characters */
+const int welcome_screen_duration_ms = 2000;  /* Welcome scren duration in ms */
 
 /* Constants */
 const int lcd_i2c_addr = 0x27;
@@ -12,17 +13,24 @@ LiquidCrystal_I2C lcd(lcd_i2c_addr, lcd_width, lcd_height);
 
 void setup() {
   configure_lcd();
+  welcome_screen();
 }
 
 void configure_lcd() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
+}
 
+void welcome_screen() {
   lcd.setCursor(3, 0);
   lcd.print("SUPER DASH");
   lcd.setCursor(6, 1);
   lcd.print("2000");
+
+  delay(welcome_screen_duration_ms);
+
+  lcd.clear();
 }
 
 void loop() {
